b2b1b55a8a3542b1831b59c11164aa024ff178d7
Stephan <Stephan@LAPTOP-STEPHAN>
2018-12-23 18:53:26 +0100

    Capture screen content

--- UserTest/Program.cs
+++ UserTest/Program.cs
@@ -1,9 +1,9 @@
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using OpenQA.Selenium.Support.UI;
 
 namespace UserTest
 {
@@ -14,6 +14,12 @@ namespace UserTest
 
             var driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://www.nu.nl");
+
+            ITakesScreenshot ssdriver = driver as ITakesScreenshot;
+            Screenshot screenshot = ssdriver.GetScreenshot();
+            string saveLocation = @"C:\test\SearchInput.png"; 
+            screenshot.SaveAsFile(saveLocation);
+
             driver.Quit();
 
         }
